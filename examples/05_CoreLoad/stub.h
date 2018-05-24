c99b7609f0050407f7712ea8f83f21315c2b7f6f
miguerdane <39587740+miguerdane@users.noreply.github.com>
2018-05-24 18:04:10 +0200

    Update stub.h

--- examples/05_CoreLoad/stub.h
+++ examples/05_CoreLoad/stub.h
@@ -9,6 +9,6 @@ extern void clock_getres(int a, struct timespec* b);
 */
 #ifndef STUB_CORELOAD_H_
 #define STUB_CORELOAD_H_
-#include <time.h>
-extern int Pclock_gettime(long a, struct timespec* b);
+//#include <time.h>
+//extern int Pclock_gettime(long a, struct timespec* b);
 #endif STUB_CORELOAD_H_
