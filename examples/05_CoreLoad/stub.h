56f8ecacc22d1431426a569701191f2f2060bf82
miguerdane <39587740+miguerdane@users.noreply.github.com>
2018-05-24 17:47:38 +0200

    Update stub.h

--- examples/05_CoreLoad/stub.h
+++ examples/05_CoreLoad/stub.h
@@ -4,6 +4,8 @@ double tv_nsec;
 double tv_sec;
 }timespec;
                   
-extern void clock_gettime(long a, struct timespec* b);
+
 extern void clock_getres(int a, struct timespec* b);
 */
+
+extern int Pclock_gettime(long a, struct timespec* b);
